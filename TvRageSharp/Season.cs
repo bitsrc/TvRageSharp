cd65d079f4125eb8a3681b60f5d5d14b0034dcbf
phate408 <phate408@gmail.com>
2014-08-28 02:20:23 -0700

    Set up basic class layout

---
+++ TvRageSharp/Season.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TvRageSharp
+{
+    /// <summary>
+    /// A season groups together episodes within a show.
+    /// </summary>
+    class Season
+    {
+    }
+}
